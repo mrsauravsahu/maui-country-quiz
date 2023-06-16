d328567bc405f6a38e7aecc03c04a64f6f114692
Saurav Sahu <mrsauravsahu@outlook.com>
2023-06-16 20:35:01 +0530

    feat: basic first screen in MainPage

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace CountryQuiz;
+﻿using Microsoft.Maui.Controls;
+
+namespace CountryQuiz;
 
 public partial class MainPage : ContentPage
 {
@@ -8,5 +10,18 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
+    async void ContentPage_Loaded(System.Object sender, System.EventArgs e)
+    {
+        var rotator = 1;
+        uint timeMs = 24000;
+        while (true)
+        {
+            await Task.WhenAny(
+                CountryLabel1.TranslateTo(rotator * Application.Current.MainPage.Width, CountryLabel1.Y, timeMs, Easing.Linear),
+                CountryLabel2.TranslateTo(-1 * rotator * Application.Current.MainPage.Width, CountryLabel1.Y, timeMs, Easing.Linear),
+                CountryLabel3.TranslateTo(rotator * Application.Current.MainPage.Width, CountryLabel1.Y, timeMs, Easing.Linear)
+            );
+            rotator *= -1;
+        }
+    }
 }
-
